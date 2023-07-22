ff35bd9967b5b30aa9bd4a7ed9abac0cdbeb9a47
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-07-22 11:47:54 +0100

    Let's get this party started! 🎉

---
+++ android/app/src/main/kotlin/com/example/godwin_ebri_portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.godwin_ebri_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:godwin_ebri_portfolio/Application/app/main_app.dart';
+
+void main() {
+  runApp(const MainApp());
+}
